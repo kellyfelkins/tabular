4e1720501d0757792327fdd5be5aec017416d1c1
Kelly Felkins <kelly@restlater.com>
2019-06-01 17:34:47 -0700

    add tests as reader functions

--- lib/tabular.ex
+++ lib/tabular.ex
@@ -3,16 +3,103 @@ defmodule Tabular do
   Documentation for Tabular.
   """
 
-  @doc """
-  Hello world.
+  @row_splitter_re ~r/^\s*[|+]--[-+|]+$/
 
-  ## Examples
+  def to_list_of_maps(ascii_table) do
+    [headers | rows] = to_list_of_lists(ascii_table)
+    headers_as_atoms = Enum.map(headers, &String.to_atom(&1))
 
-      iex> Tabular.hello()
-      :world
-
-  """
-  def hello do
-    :world
+    rows
+    |> Enum.map(fn row ->
+      Enum.zip(headers_as_atoms, row)
+      |> Map.new()
+    end)
   end
+
+  def to_list_of_lists_no_hd(ascii_table) do
+    ascii_table
+    |> to_list_of_lists
+    |> Enum.drop(1)
+  end
+
+  def to_list_of_lists(ascii_table) do
+    ascii_table
+    |> lines()
+    |> cell_line_groups()
+    |> trimmed_and_grouped_cell_contents()
+    |> folded_cell_contents()
+    |> specials()
+  end
+
+  def lines(ascii_table) do
+    ascii_table
+    |> String.trim()
+    |> String.split("\n")
+  end
+
+  def cell_line_groups(lines) do
+    lines
+    |> Enum.chunk_by(fn line ->
+      Regex.match?(@row_splitter_re, line)
+    end)
+    |> Enum.filter(fn group ->
+      !(length(group) == 1 &&
+          Regex.match?(@row_splitter_re, hd(group)))
+    end)
+  end
+
+  def trimmed_and_grouped_cell_contents(line_groups) do
+    line_groups
+    |> Enum.map(fn line_group ->
+      line_group
+      |> Enum.map(fn line ->
+        [_all, trimmed_line] = Regex.run(~r/^\s*\|\s*(.*)\s*\|\s*$/, line)
+
+        trimmed_line
+        |> String.split("|")
+        |> Enum.map(fn cell_content ->
+          String.trim(cell_content)
+        end)
+      end)
+    end)
+  end
+
+  def folded_cell_contents(grouped_cell_contents) do
+    grouped_cell_contents
+    |> Enum.map(fn row_contents ->
+      row_contents
+      |> fold_contents_if_necessary
+    end)
+  end
+
+  def fold_contents_if_necessary(row_contents) when length(row_contents) > 1 do
+    Enum.zip(row_contents)
+    |> Enum.map(fn foldables ->
+      foldables
+      |> Tuple.to_list()
+      |> Enum.filter(fn text -> text != "" end)
+      |> Enum.join(" ")
+    end)
+  end
+
+  def fold_contents_if_necessary(row_contents) do
+    hd(row_contents)
+  end
+
+  def specials(rows_of_columns) do
+    rows_of_columns
+    |> Enum.map(fn row ->
+      row
+      |> Enum.map(fn text ->
+        special(text)
+      end)
+    end)
+  end
+
+  def special("true"), do: true
+  def special("false"), do: false
+  def special("nil"), do: nil
+  def special(""), do: nil
+  def special(":" <> rest), do: String.to_atom(rest)
+  def special(not_special), do: not_special
 end
--- test/tabular_test.exs
+++ test/tabular_test.exs
@@ -1,8 +1,178 @@
 defmodule TabularTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest Tabular
 
-  test "greets the world" do
-    assert Tabular.hello() == :world
+  describe "lines" do
+    test "splits and trims lines" do
+      data_as_table = """
+      line 1
+      line 2
+      line 3
+      """
+
+      expected = [
+        "line 1",
+        "line 2",
+        "line 3"
+      ]
+
+      assert Tabular.lines(data_as_table) == expected
+    end
+  end
+
+  describe "cell_line_groups" do
+    test "extracts lines with cell contents and groups them" do
+      data_as_lines = [
+        "+-----------+--------------------+",
+        "| name      | dob                |",
+        "+-----------+--------------------+",
+        "| Malcolm   | September 20, 2468 |",
+        "| Reynolds  |                    |",
+        "+-----------+--------------------+",
+        "| Jane      |                    |",
+        "+-----------+--------------------+"
+      ]
+
+      expected = [
+        ["| name      | dob                |"],
+        ["| Malcolm   | September 20, 2468 |", "| Reynolds  |                    |"],
+        ["| Jane      |                    |"]
+      ]
+
+      assert Tabular.cell_line_groups(data_as_lines) == expected
+    end
+  end
+
+  describe "trimmed_and_grouped_cell_contents" do
+    test "returns cleaned strings" do
+      data_as_line_groups = [
+        ["| name      | dob                |"],
+        ["| Malcolm   | September 20, 2468 |", "| Reynolds  |                    |"],
+        ["| Jane      |                    |"]
+      ]
+
+      expected = [
+        [["name", "dob"]],
+        [["Malcolm", "September 20, 2468"], ["Reynolds", ""]],
+        [["Jane", ""]]
+      ]
+
+      assert Tabular.trimmed_and_grouped_cell_contents(data_as_line_groups) == expected
+    end
+  end
+
+  describe "folded_cell_contents" do
+    test "returns folded strings" do
+      data = [
+        [["name", "dob"]],
+        [["Malcolm", "September 20, 2468"], ["Reynolds", ""]],
+        [["Jane", ""]]
+      ]
+
+      expected = [
+        ["name", "dob"],
+        ["Malcolm Reynolds", "September 20, 2468"],
+        ["Jane", ""]
+      ]
+
+      assert Tabular.folded_cell_contents(data) == expected
+    end
+  end
+
+  describe "to_list_of_lists" do
+    test "returns values as a list of lists" do
+      data_as_table = """
+            +-----------+--------------------+--------------+
+            | name      | dob                | predictable? |
+            +-----------+--------------------+--------------+
+            | Malcolm   | September 20, 2468 | false        |
+            | Reynolds  |                    |              |
+            +-----------+--------------------+--------------+
+            | Zoe       | February 15, 2484  |              |
+            | Washburne |                    |              |
+            +-----------+--------------------+--------------+
+            | Jane      | :unknown           | true         |
+            +-----------+--------------------+--------------+
+            | Derrial   | nil                | true         |
+            | Book      |                    |              |
+            +-----------+--------------------+--------------+
+      """
+
+      actual = Tabular.to_list_of_lists(data_as_table)
+
+      expected = [
+        ["name", "dob", "predictable?"],
+        ["Malcolm Reynolds", "September 20, 2468", false],
+        ["Zoe Washburne", "February 15, 2484", nil],
+        ["Jane", :unknown, true],
+        ["Derrial Book", nil, true]
+      ]
+
+      assert actual == expected
+    end
+  end
+
+  describe "to_list_of_lists_no_hd" do
+    test "returns values as a list of lists without the header" do
+      data_as_table = """
+            +-----------+--------------------+--------------+
+            | name      | dob                | predictable? |
+            +-----------+--------------------+--------------+
+            | Malcolm   | September 20, 2468 | false        |
+            | Reynolds  |                    |              |
+            +-----------+--------------------+--------------+
+            | Zoe       | February 15, 2484  |              |
+            | Washburne |                    |              |
+            +-----------+--------------------+--------------+
+            | Jane      | :unknown           | true         |
+            +-----------+--------------------+--------------+
+            | Derrial   | nil                | true         |
+            | Book      |                    |              |
+            +-----------+--------------------+--------------+
+      """
+
+      actual = Tabular.to_list_of_lists_no_hd(data_as_table)
+
+      expected = [
+        ["Malcolm Reynolds", "September 20, 2468", false],
+        ["Zoe Washburne", "February 15, 2484", nil],
+        ["Jane", :unknown, true],
+        ["Derrial Book", nil, true]
+      ]
+
+      assert actual == expected
+    end
+  end
+
+  describe "to_list_of_maps" do
+    test "returns values as a list of maps" do
+      data_as_table = """
+            +-----------+--------------------+--------------+
+            | name      | dob                | predictable? |
+            +-----------+--------------------+--------------+
+            | Malcolm   | September 20, 2468 | false        |
+            | Reynolds  |                    |              |
+            +-----------+--------------------+--------------+
+            | Zoe       | February 15, 2484  |              |
+            | Washburne |                    |              |
+            +-----------+--------------------+--------------+
+            | Jane      | :unknown           | true         |
+            +-----------+--------------------+--------------+
+            | Derrial   | nil                | true         |
+            | Book      |                    |              |
+            +-----------+--------------------+--------------+
+      """
+
+      actual = Tabular.to_list_of_maps(data_as_table)
+
+      expected = [
+        %{name: "Malcolm Reynolds", dob: "September 20, 2468", predictable?: false},
+        %{name: "Zoe Washburne", dob: "February 15, 2484", predictable?: nil},
+        %{name: "Jane", dob: :unknown, predictable?: true},
+        %{name: "Derrial Book", dob: nil, predictable?: true}
+      ]
+
+      assert actual == expected
+    end
   end
 end
