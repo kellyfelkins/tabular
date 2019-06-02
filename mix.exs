ca0a442fa05fc4c3e39e19f34ef9c28d1a9e3fc0
Kelly Felkins <kelly@restlater.com>
2019-06-02 16:30:34 -0700

    changes required for hex.pm

--- mix.exs
+++ mix.exs
@@ -4,12 +4,13 @@ defmodule Tabular.MixProject do
   def project do
     [
       app: :tabular,
+      description: description(),
       version: "0.1.0",
       elixir: "~> 1.8",
       deps: deps(),
       name: "Tabular",
       source_url: "https://github.com/kellyfelkins/tabular",
-      homepage_url: "https://github.com/kellyfelkins/tabular",
+      package: package(),
       docs: [
         main: "Tabular",
         extras: ["README.md"]
@@ -20,6 +21,19 @@ defmodule Tabular.MixProject do
   defp deps do
     [
       {:ex_doc, "~> 0.19", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description do
+    "Tabular converts an ascii table string into either a list of lists or a list of maps"
+  end
+
+  defp package do
+    [
+      licenses: ["BlueOak-1.0.0"],
+      links: %{
+        "GitHub" => "https://github.com/kellyfelkins/tabular"
+      }
     ]
   end
 end
