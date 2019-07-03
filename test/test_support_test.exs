a9a13321fc8f5bfa7e25023cdf7e2b0344b4ed65
Kelly Felkins <kelly@restlater.com>
2019-07-02 21:32:48 -0700

    Improve readme and tests.

--- test/test_support_test.exs
+++ test/test_support_test.exs
@@ -2,7 +2,7 @@ defmodule Tabular.TestSupportTest do
   use ExUnit.Case, async: true
 
   describe "equal?" do
-    test "when there is a false value in any one of the rows, returns false" do
+    test "when there is a false value in any cell, returns false" do
       results_table = [
         [true, true, true],
         [true, false, true],
@@ -11,25 +11,10 @@ defmodule Tabular.TestSupportTest do
 
       refute Tabular.TestSupport.equal?(results_table)
     end
+  end
 
-    test "is true when tables match" do
-      table1 = """
-            +---------+--------------------+
-            | name    | dob                |
-            +---------+--------------------+
-            | Malcolm | September 20, 2468 |
-            +---------+--------------------+
-            | Zoe     | February 15, 2484  |
-            +---------+--------------------+
-      """
-
-      table2 = table1
-
-      assert Tabular.TestSupport.compare(table1, table2)
-             |> Tabular.TestSupport.equal?()
-    end
-
-    test "is false when any cell does not match" do
+  describe "compare" do
+    test "creates a matrix indicating non-matching cells with `false`" do
       table1 = """
             +---------+--------------------+
             | name    | dob                |
@@ -50,11 +35,15 @@ defmodule Tabular.TestSupportTest do
             +---------+--------------------+
       """
 
-      refute Tabular.TestSupport.compare(table1, table2)
-             |> Tabular.TestSupport.equal?()
+      results_table = [
+        [false, true],
+        [true, true]
+      ]
+
+      assert Tabular.TestSupport.compare(table1, table2) == results_table
     end
 
-    test "is true when all cell functions result in true" do
+    test "uses comparator functions to compare cells" do
       table1 = """
             +---------+-------+
             | name    | count |
@@ -71,41 +60,19 @@ defmodule Tabular.TestSupportTest do
             +---------+-------+
             | Malcolm | 11    |
             +---------+-------+
-            | Zoe     | 4     |
+            | Zoe     | 20    |
             +---------+-------+
       """
+
+      results_table = [
+        [true, true],
+        [true, false]
+      ]
 
       comparators = %{"count" => &near/2}
 
-      assert Tabular.TestSupport.compare(table1, table2, comparators: comparators)
-             |> Tabular.TestSupport.equal?()
-    end
-
-    test "is false when any cell function result in false" do
-      table1 = """
-            +---------+-------+
-            | name    | count |
-            +---------+-------+
-            | Malcolm | 10    |
-            +---------+-------+
-            | Zoe     | 5     |
-            +---------+-------+
-      """
-
-      table2 = """
-            +---------+-------+
-            | name    | count |
-            +---------+-------+
-            | Malcolm | 31    |
-            +---------+-------+
-            | Zoe     | 4     |
-            +---------+-------+
-      """
-
-      comparators = %{"count" => &near/2}
-
-      refute Tabular.TestSupport.compare(table1, table2, comparators: comparators)
-             |> Tabular.TestSupport.equal?()
+      assert Tabular.TestSupport.compare(table1, table2, comparators: comparators) ==
+               results_table
     end
   end
 
